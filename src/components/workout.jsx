45064a8d5c7d4df6ca08b681b5e0fc8981adef05
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-07-24 04:57:09 -0600

    Created a component for each workout and displayed the notes for each workout on the dashboard

---
+++ src/components/workout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WorkoutLog({ workoutLog }) {
+  return <div>{workoutLog.note}</div>;
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import useWorkoutApi from "../hooks/useWorkoutApi";
 import useWorkoutLogsApi from "../hooks/useWorkoutLogsApi";
+import WorkoutLog from "../components/workout.jsx";
 
 export default function DashboardPage() {
   const { getAllWorkouts } = useWorkoutApi();
   const { getAllWorkoutLogs } = useWorkoutLogsApi();
   const [workouts, setWorkouts] = useState(null);
   const [workoutLogs, setWorkoutLogs] = useState(null);
-  const { loading, setLoading } = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(async () => {
     const getAllWorkoutsEffect = async () => {
@@ -32,9 +33,28 @@ export default function DashboardPage() {
     getAllWorkoutLogsEffect();
   }, []);
 
+  useEffect(() => {
+    if (workoutLogs) {
+      setLoading(false);
+    }
+  }, [workoutLogs]);
+
+  let WorkoutLogsList;
+  if (loading) {
+    WorkoutLogsList = null;
+  } else {
+    WorkoutLogsList = (
+      <div>
+        {workoutLogs.map((workoutLog) => (
+          <WorkoutLog workoutLog={workoutLog} key={workoutLog.workout_log_id} />
+        ))}
+      </div>
+    );
+  }
   return (
     <section>
       <h1>Dashboard Page</h1>
+      {WorkoutLogsList}
     </section>
   );
 }
