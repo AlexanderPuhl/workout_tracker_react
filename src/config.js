8f22953290c8844bfc3b66e011d97c03c0bb9642
Chris Gantz <chris.gonzalez836@gmail.com>
2021-07-24 17:11:36 -0700

    removed merge conflicts

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-/* eslint-disable import/prefer-default-export */
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+export default process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
