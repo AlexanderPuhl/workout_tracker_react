14ecd30a26c3f00ec1fff742c50e8b9ff7742d35
Chris Gantz <chris.gonzalez836@gmail.com>
2021-08-02 10:18:33 -0700

    added a spinner for loading effects, created a general fetch api that accepts params, finished profile component

---
+++ src/components/spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Spinner.css';
+
+const LoadSpinner = () => (
+  <div className="lds-facebook"><div></div><div></div><div></div></div>
+);
+
+export default LoadSpinner;
