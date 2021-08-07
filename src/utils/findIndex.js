5bedb9a94f3c00ccdf14122022552a2b191615a5
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-08-07 15:08:56 -0600

    Added notes to the utils/findIndex.js file explaining the purpose and how to use it

--- src/utils/findIndex.js
+++ src/utils/findIndex.js
@@ -1,3 +1,11 @@
+// This will find the position in an array of whatever you're looking for using an ID of some sort.
+// You give this an array of objects, the key to look for like workout_id, and then the value of whatever ID is being passed in.
+
+// Example: 
+
+// import findWithAttr from "../utils/findIndex";
+// const index = findWithAttr(allWorkoutLogs, "workout_log_id", workoutLogID);
+
 export default function findWithAttr(array, attr, value) {
   for (let i = 0; i < array.length; i += 1) {
     if (array[i][attr] === value) {
